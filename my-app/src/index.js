0891e7d9eec3b4f31339e429a5a6c785e616421c
Mykyta <nikitya93@gmail.com>
2023-03-18 22:06:49 -0400

    elements_and_components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,28 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Header } from './App';
 
-// const elem = <h2>Hello Word!</h2>;
-// const elem = React.createElement('h2', {className: 'greeti ngs'}, 'Hello World!');
 
-const text = 'Hello World!'
-
-const elem = (
-    <div>
-      <h2 className='Text'>Текст: {text}</h2>
-      <input type="text" />
-      <label htmlFor=""></label>
-      <button tabIndex={0}>Click</button>
-      <button/>
-    </div>
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  /*<React.StrictMode>
+  <React.StrictMode>
     <App />
-  </React.StrictMode>*/
-  elem,
+  </React.StrictMode>
 );
 
 
