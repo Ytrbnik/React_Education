08ceacc8111ce8e5fd678c8e1adf3adff1efd684
Mykyta <nikitya93@gmail.com>
2023-03-25 18:37:48 -0400

    styled_Commponent

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,14 +2,20 @@ import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Button } from './App';
+import styled from 'styled-components';
 
-
-
+const BigButton = styled(Button)`
+  margin: 0 auto;
+  width: 245px;
+  text-align: center;
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <App />
+    <BigButton as="a">Отправить отчет</BigButton>
   </StrictMode>
 );
 
