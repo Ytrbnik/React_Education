4e1e7c0bcd4ea5d4aa0a3369ab4cdb69a5417a83
Mykyta <nikitya93@gmail.com>
2023-04-08 22:35:05 -0400

    Render-props pattern

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -99,9 +99,44 @@ const HelloGreatings = () => {
   )
 }
 
+const Message = (props) => {
+  return (
+    <h2>The counter is {props.counter}</h2>
+  )
+}
+
+class Counter extends Component {
+  state = {
+    counter: 0
+  }
+
+  changeCounter = () => {
+    this.setState(({counter}) => ({
+      counter: counter + 1
+    }))
+  }
+
+  render() {
+    return (
+      <>
+        <button
+          className='btn btn-primary'
+          onClick={this.changeCounter}>
+          Click me!
+        </button>
+        {this.props.render(this.state.counter)}
+      </>
+    )
+  }
+}
+
 function App() {
   return (
     <Wrapper>
+
+      <Counter render={counter => {
+        <Message counter={counter}/>
+      }}/>
       <HelloGreatings/>
       <DynamicGreating color={'primary'}>
         <h2>This weel was hard</h2>
